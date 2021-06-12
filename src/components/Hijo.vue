d31aa5d116914ef2937aedf5a7c78302be734145
Irene <ireensc@gmail.com>
2021-06-12 23:43:15 +0200

    ejercicio Vuem10.2.2 subido

---
+++ src/components/Hijo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="hijoDiv" >
+        <h4>Item {{items}}:</h4>
+        <button @click="numero++">{{numero}}</button>
+    </div>
+</template>
+
+
+<script>
+export default{
+    name: "Hijo",
+    props: ['numero', "items"],
+    // data(){
+    //     return{
+    //         items: ["Item 1", "Item 2", "Item 3"]
+    //     }
+    // },
+    // mounted(){
+    //     this.$emit("itemsHijo", this.items)
+    // }
+}
+</script>
+
+<style>
+.hijoDiv{
+    background: white;
+    width: 30%;
+    margin:auto;
+    border:3px solid #40B983;   
+    margin-bottom: 20px;
+}
+
+button{
+    background: #40B983;
+    border:none;
+    border-radius: 5px;
+    padding:10px;
+    margin-bottom: 10px; 
+}
+</style>
---
+++ src/components/Padre.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="padreDiv">
+        <h2>Componente Padre:</h2>
+        <Hijo v-for="(item, index) of itemsPadre" :key="index" :items="item" :numero="numeroPadre"></Hijo>  
+    </div>
+</template>
+
+
+<script>
+import Hijo from "./Hijo"
+
+export default{
+    name: "Padre",
+    components: {Hijo},
+    data(){
+        return {
+            numeroPadre: 0,
+            itemsPadre: [1,2,3]
+        }
+    }
+}
+</script>
+
+
+<style>
+.padreDiv{
+    background: #D7DBDF;
+    border:3px solid #40B983;
+}
+</style>
